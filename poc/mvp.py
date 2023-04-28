d7a4a97eeed6bd645669e9fe3092df259d4cc00b
maxmekiska <maxmekiska@gmail.com>
2023-04-28 01:40:38 +0100

    proof of concept

---
+++ poc/mvp.py
@@ -0,0 +1,105 @@
+import logging
+import inspect
+import traceback
+import re
+
+from memory import shortTermMemory
+
+
+suggested_code = """
+def my_function(x, y, z):
+    try:
+        result = x / y + z
+        return result
+    except ZeroDivisionError:
+        return None
+"""
+
+def remove_trace_lines(trace_error: str) -> str:
+    pattern = r"\b\w*line\s\d+\w*\b"
+    output_str = re.sub(pattern, "", trace_error)
+    return output_str
+
+def remove_wrapper_name(source_code: str) -> str:
+    start_index = source_code.index("def")
+    source_code = source_code[start_index:]
+    return source_code
+
+
+
+def request_correction() -> str:
+    return suggested_code
+
+def return_source_code(func: callable) -> str:
+    source = (inspect.getsource(func))
+    return source
+
+def return_input_arguments(func: callable, *args, **kwargs) -> dict:
+    signature = inspect.signature(func)
+    bound_args = signature.bind(*args, **kwargs)
+    input_args = {k: bound_args.arguments[k] for k in signature.parameters.keys()}
+    return input_args
+
+
+def function_logger(func):
+    def wrapper(*args, **kwargs):
+
+
+        try:
+            result = func(*args, **kwargs)
+            return result
+        
+
+        except Exception as e:
+            logging.exception(e)
+            trace = traceback.format_exc()
+            trace = remove_trace_lines(trace) # pass to ChatGPT
+
+            input_args = return_input_arguments(func, *args, **kwargs) # pass to ChatGPT
+            source = return_source_code(func)
+            source = remove_wrapper_name(source) # pass to ChatGPT
+
+            logging.warning(f'Input arguments: {input_args}')
+            logging.warning(f'\nSource Code: \n {source}')
+
+            if trace in shortTermMemory.keys():
+                logging.warning('Correction already in memory')
+                suggested_code = shortTermMemory[trace]
+
+
+            else:
+                logging.warning('Requesting correction')
+                suggested_code = request_correction()
+
+            shortTermMemory[trace] = suggested_code
+
+            logging.warning(f'Short term memory: \n Suggested code: {suggested_code} \n Traceback: {trace}')
+
+            compiled_code = compile(suggested_code, '<string>', 'exec')
+
+            exec(compiled_code, input_args)
+
+            new_function = input_args[func.__name__]
+
+
+
+            return new_function(*args, **kwargs)
+        
+
+    return wrapper
+
+
+
+
+if __name__ == "__main__":
+
+
+    @function_logger
+    def my_function(x, y, z):
+        """function to add three variables."""
+        result = x + y + z + o
+        return result
+
+    print(my_function(x = 1, y = 2, z= 2))
+    print(my_function(x = 2, y = 8, z= 2))
+    print(my_function(x = 9, y = 8, z= 2))
