a903919af7eeed51785ce8f218b408ffa9bbea88
maxmekiska <maxmekiska@gmail.com>
2023-05-02 01:10:46 +0100

    minor fixes to model configuration

--- fukkatsu/utils/llmfix.py
+++ fukkatsu/utils/llmfix.py
@@ -1,9 +1,7 @@
 import os
-
 import openai
 
 openai.api_key = os.environ.get("API_KEY")
-model_temp = os.environ.get("MODEL_TEMP")
 
 MODEL = "gpt-3.5-turbo"
 
@@ -12,17 +10,16 @@ CONTEXT = """Repair and improve the following faulty function: """
 
 
 def request_correction(inputs: str, faulty_function: str, error_trace: str) -> str:
-    prompt = f"{CONTEXT}\n\n{faulty_function}\n\nThe function received the following inputs:\n\n{inputs}\n\nAnd returned the following error trace:\n\n{error_trace}\n\nRespond only with one code box, do not provide only the corrected function do not provide any examples."
+    prompt = f"{CONTEXT}\n\n{faulty_function}\n\nThe function received the following inputs:\n\n{inputs}\n\nAnd returned the following error trace:\n\n{error_trace}\n\nRespond with exact one code box. Provide only the corrected function. Do not provide examples."
 
-    model_engine = MODEL
 
     response = openai.Completion.create(
-        engine=model_engine,
+        engine=MODEL,
         prompt=prompt,
         max_tokens=1024,
         n=1,
         stop=None,
-        temperature=model_temp,
+        temperature=0.1,
     )
 
     corrected_function = response["choices"][0]["message"]["content"].strip()
