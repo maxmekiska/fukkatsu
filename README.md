2a3877829f207492e0e6f59605993d457047dbda
maxmekiska <maxmekiska@gmail.com>
2023-05-04 00:35:00 +0100

    implemented extra life logic in the mvp code; TODO: implement in production code

--- README.md
+++ README.md
@@ -7,14 +7,18 @@ This is a proof of concept for a library that will leverage LLMs to dynamically 
 Insane? Yes. Possible? Maybe. Fun? Definitely.
 
 
-Here is a representation of what we are trying to do: https://giphy.com/gifs/tire-kNRqJCLOe6ri8/fullscreen 
+Here is a representation of what I am trying to do: https://giphy.com/gifs/tire-kNRqJCLOe6ri8/fullscreen 
 
+
+Please note that this concept currently applies only for scriping languages such as python and not for compiled languages such as C++. The very nature of scripting languages allows us to dynamically change the code during runtime.
 
 ## MVP
 
 You can find a MVP within the `poc` folder. You can simpy run the code via `python mvp.py`. The code will simulate a failing function, which will be repaird during execution. The mvp.py code will not request a correction to an OpenAi LLM but simply ueses a mock corrected function.
 
-### Example:
+### Foundation
+
+#### Example:
 
 - we have a function called `my_function` which takes accepts three arguments: 'x', 'y', 'z' and returns a value calculated via `x / y + z`
 - lets assume the function `my_function` accidentally receives the value 0 for the argument 'y'
@@ -39,8 +43,29 @@ def my_function(x, y, z):
     return result
 
 print(my_function(x = 1, y = 0, z= 2)) # would fail, but is corrected and returns 2
-print(my_function(x = 2, y = 0, z= 10)) # would fail, but is corrected and returns 2
+print(my_function(x = 2, y = 0, z= 10)) # would fail, but is corrected and returns 10
 print(my_function(x = 9, y = 1, z= 2) + 10 )  # would not fail, returns 21.0
 ```
 
-Please note, the example in the above is trivial however LLMs such as `gpt-3.5-turbo` are able to correct more complex functions. Once the library is more mature, more experiments and examples will show if such a use case for LLMs is worthwhile .
+Please note, the example in the above is trivial however LLMs such as `gpt-3.5-turbo` are able to correct more complex functions. Once the library is more mature, more experiments and examples will show if such a use case for LLMs is worthwhile.
+
+
+### Extra life
+
+Here is again a representation of what I am trying to achieve: https://media.tenor.com/r5nBe8Ft6yEAAAAC/ready-player-one-extra-life.gif
+
+The code mvp code offers now the concept of `extra lives`. The idea of extra lives is to allow the user to define, per function, how often a LLM should attempt to fix errors. This will allow LLMs to futher explore other paths of fixing the code at runtime however it will also make sure to bound the runtime of the LLM.
+
+#### Example:
+
+```python
+@mvp_reanimate(lives=2)
+def my_function(x, y, z):
+    """
+    function to divide x by y and add to the result z. Should return z if y is 0.
+    """
+    result = x / y + z
+    return result
+```
+
+The above example will allow the LLM to attempt to fix the function twice. If the LLM fails to fix the function after two attempts, a `flatline error` will be raised which indicates that the LLM was not able to fix the function during runtime.
--- poc/mvp.py
+++ poc/mvp.py
@@ -1,3 +1,4 @@
+import functools
 import logging
 import inspect
 import traceback
@@ -13,6 +14,11 @@ def my_function(x, y, z):
         return x / y + z
 """
 
+suggested_code2 = """
+def my_function(x, y, z):
+    return 42
+"""
+
 def remove_trace_lines(trace_error: str) -> str:
     pattern = r"\b\w*line\s\d+\w*\b"
     output_str = re.sub(pattern, "", trace_error)
@@ -22,8 +28,6 @@ def remove_wrapper_name(source_code: str) -> str:
     start_index = source_code.index("def")
     source_code = source_code[start_index:]
     return source_code
-
-
 
 def request_correction() -> str:
     return suggested_code
@@ -39,65 +43,76 @@ def return_input_arguments(func: callable, *args, **kwargs) -> dict:
     return input_args
 
 
-def mvp_reanimate(func):
-    def wrapper(*args, **kwargs):
+def mvp_reanimate(lives = 1):
+    def _mvp_reanimate(func):
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
 
 
-        try:
-            result = func(*args, **kwargs)
-            return result
-        
-
-        except Exception as e:
-            logging.exception(e)
-            trace = traceback.format_exc()
-            trace = remove_trace_lines(trace) # pass to ChatGPT
-
-            input_args = return_input_arguments(func, *args, **kwargs) # pass to ChatGPT
-            source = return_source_code(func)
-            source = remove_wrapper_name(source) # pass to ChatGPT
-
-            logging.warning(f'Input arguments: {input_args}')
-            logging.warning(f'\nSource Code: \n {source}')
-
-            if trace in shortTermMemory.keys():
-                logging.warning('Correction already in memory')
-                suggested_code = shortTermMemory[trace]
-
-
-            else:
-                logging.warning('Requesting correction')
-                suggested_code = request_correction()
-
-            shortTermMemory[trace] = suggested_code
-
-            logging.warning(f'Short term memory: \n Suggested code: {suggested_code} \n Traceback: {trace}')
-
-
+            try:
+                result = func(*args, **kwargs)
+                return result
             
-            global_dict = globals().copy()
-            local_dict = locals().copy()
+
+            except Exception as e:
+                logging.exception(e)
+                trace = traceback.format_exc()
+                trace = remove_trace_lines(trace) # pass to ChatGPT
+
+                input_args = return_input_arguments(func, *args, **kwargs) # pass to ChatGPT
+                source = return_source_code(func)
+                source = remove_wrapper_name(source) # pass to ChatGPT
+
+                logging.warning(f'Input arguments: {input_args}')
+                logging.warning(f'\nSource Code: \n {source}')
+
+                if trace in shortTermMemory.keys():
+                    logging.warning('Correction already in memory')
+                    suggested_code = shortTermMemory[trace]
 
 
-            compiled_code = compile(suggested_code, '<string>', 'exec')
+                else:
+                    logging.warning('Requesting correction')
+                    suggested_code = request_correction()
+
+                shortTermMemory[trace] = suggested_code
+
+                logging.warning(f'Short term memory: \n Suggested code: {suggested_code} \n Traceback: {trace}')
 
 
-            exec(compiled_code, global_dict, local_dict)
-            new_function = local_dict[func.__name__]
+                for i in range(lives):
+                    logging.warning(f'Correction attempt {i+1}')
+                    try:
+                        global_dict = globals().copy()
+                        local_dict = locals().copy()
 
 
+                        compiled_code = compile(suggested_code, '<string>', 'exec')
 
 
-            return new_function(*args, **kwargs)
-        
+                        exec(compiled_code, global_dict, local_dict)
+                        new_function = local_dict[func.__name__]
 
-    return wrapper
+                        return new_function(*args, **kwargs)
+
+                    except Exception as e:
+                        logging.exception(e)
+                        logging.warning('Correction failed, requesting new correction')
+                        suggested_code = suggested_code2
+                        shortTermMemory[trace] = suggested_code2
+                        logging.warning(f'Short term memory: \n Suggested code: {suggested_code} \n Traceback: {trace}')
+                raise Exception(f'|__|__|______ {func.__name__} flatlined')
+    
+
+        return wrapper
+    return _mvp_reanimate
 
 
 if __name__ == "__main__":
 
 
-    @mvp_reanimate
+    @mvp_reanimate(lives = 2)
     def my_function(x, y, z):
         """
         function to divide x by y and add to the result z. Should return z if y is 0.
@@ -105,13 +120,7 @@ if __name__ == "__main__":
         result = x / y + z
         return result
 
-    @mvp_reanimate
-    def my_function2(x, y):
-        """function to add two variables."""
-        result = x + y
-        return result
 
     print(my_function(x = 1, y = 0, z= 2))
     print(my_function(x = 2, y = 0, z= 10))
     print(my_function(x = 9, y = 1, z= 2) + 10 )
-    print(my_function2(x = 9, y = 8) + 10 )
